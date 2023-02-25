c943d4293cbb7b1e9850dfe85828ed4988f26e6f
Alexander <aguillenhernandez95@gmail.com>
2023-02-25 17:56:58 +0100

    Add DELELTE /users/relationship endpoint

--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import createRelationship from "../controllers/createRelationship/createRelationship.js";
+import deleteRelationShip from "../controllers/deleteRelationship/deleteRelationship.js";
 import { loginUser } from "../controllers/loginUser/loginUser.js";
 import registerUser from "../controllers/registerUser/registerUser.js";
 
 const usersRouter = Router();
 const loginEndpoint = "/login";
 const registerEndpoint = "/register";
-const createRelationEndpoint = "/relationship";
+const relationEndpoint = "/relationship";
 
 usersRouter.post(loginEndpoint, loginUser);
 usersRouter.post(registerEndpoint, registerUser);
-usersRouter.post(createRelationEndpoint, createRelationship);
+usersRouter.post(relationEndpoint, createRelationship);
+usersRouter.delete(relationEndpoint, deleteRelationShip);
 
 export default usersRouter;
