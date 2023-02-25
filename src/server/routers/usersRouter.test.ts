7acd83ba661fcb72378abfb945d71ea44bf6f3f1
Alexander <aguillenhernandez95@gmail.com>
2023-02-25 15:25:25 +0100

    Test POST /users/relationship endpoint

--- src/server/routers/usersRouter.test.ts
+++ src/server/routers/usersRouter.test.ts
@@ -1,11 +1,13 @@
 import request from "supertest";
 import jwt from "jsonwebtoken";
 import { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 import connectToDatabase from "../../database/connectToDatabase.js";
 import User from "../../database/models/User.js";
 import app from "../index.js";
 import {
+  type NewRelationshipResponseStructure,
+  type CreateRelationShipStructure,
   type ErrorResponseStructure,
   type TokenStructure,
   type UserCredentials,
@@ -23,11 +25,6 @@ afterAll(async () => {
   await server.stop();
 });
 
-afterEach(async () => {
-  await User.deleteMany();
-  jest.clearAllMocks();
-});
-
 describe("Given POST /users/login endpoint", () => {
   const user: UserCredentials = {
     username: "Alexander",
@@ -37,6 +34,11 @@ describe("Given POST /users/login endpoint", () => {
 
   beforeAll(async () => {
     await User.create(user);
+  });
+
+  afterEach(async () => {
+    await User.deleteMany();
+    jest.clearAllMocks();
   });
 
   describe("When a request is sent to it with username 'Alexander', password 'elGrande', and email 'coso@gmail.com'", () => {
@@ -72,6 +74,64 @@ describe("Given POST /users/login endpoint", () => {
         .expect(401);
 
       expect(response.body).toStrictEqual(credentialsError);
+    });
+  });
+});
+
+describe("Given a POST /users/relationship", () => {
+  beforeAll(async () => {
+    const Alexander = new User({
+      username: "Alexander",
+      password: "elGrande",
+      email: "aguillenhernandez@gmail.com",
+    });
+
+    const Alexandra = new User({
+      username: "Alexandra",
+      password: "laGrande",
+      email: "alexaguillenhernandez@gmail.com",
+    });
+
+    Alexander._id = new Types.ObjectId("63fa1109f2df6dd57752a24e");
+    Alexandra._id = new Types.ObjectId("63fa1117ee90ea8431f81764");
+
+    await Alexander.save();
+    await Alexandra.save();
+  });
+
+  const createRelationRequest: CreateRelationShipStructure = {
+    userId: "63fa1109f2df6dd57752a24e",
+    user2Id: "63fa1117ee90ea8431f81764",
+    relationtype: "friend",
+  };
+
+  describe("When a request is sent to it with userId '63fa1109f2df6dd57752a24e' user2Id '63fa1117ee90ea8431f81764' and relationType 'friend'", () => {
+    test("Then it should respond with status 201 and message 'New relationship: Alexander & Alexandra (friend)'", async () => {
+      const newRelationShip: NewRelationshipResponseStructure = {
+        response: "New relationship: Alexander & Alexandra (friend)",
+      };
+
+      const response = await request(app)
+        .post("/users/relationship")
+        .send(createRelationRequest)
+        .expect(201);
+
+      expect(response.body).toStrictEqual(newRelationShip);
+    });
+  });
+
+  describe("When a new request is sent to it with userId '63fa1109f2df6dd57752a24e' user2Id '63fa1117ee90ea8431f81764' and relationType 'friend'", () => {
+    test("Then it should respond with status 409 and message 'A relationship between this users already exists'", async () => {
+      const error: ErrorResponseStructure = {
+        error: "A relationship between this users already exists",
+      };
+
+      const response = await request(app)
+        .post("/users/relationship")
+        .send(createRelationRequest)
+        .expect(409);
+
+      expect(response.body).toStrictEqual(error);
     });
   });
 });
--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
+import createRelationship from "../controllers/createRelationship/createRelationship.js";
 import { loginUser } from "../controllers/loginUser/loginUser.js";
 import registerUser from "../controllers/registerUser/registerUser.js";
 
 const usersRouter = Router();
 const loginEndpoint = "/login";
 const registerEndpoint = "/register";
+const createRelationEndpoint = "/relationship";
 
 usersRouter.post(loginEndpoint, loginUser);
 usersRouter.post(registerEndpoint, registerUser);
+usersRouter.post(createRelationEndpoint, createRelationship);
 
 export default usersRouter;
